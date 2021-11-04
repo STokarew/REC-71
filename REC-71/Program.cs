667cb8a33fa308aec892ca319dc9931301a786bc
сергей токарев <sseregatokarew@gmail.com>
2021-11-04 18:09:19 +0700

    Добавьте файлы проекта.

---
+++ REC-71/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+namespace REC_71
+
+{
+    class Program
+    {
+        static string CleanString(string strIn)
+        {
+            return Regex.Replace(strIn, @"[\W*]", "");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            Console.WriteLine($"Результирующая строка: {CleanString(Console.ReadLine())}");
+            Console.ReadLine();
+
+        }
+    }
+}
